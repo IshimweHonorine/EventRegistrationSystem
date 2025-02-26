211c25f03c5aa67c27b9c9d3147a0ad09d9a37f2
IshimweHonorine <104136109+IshimweHonorine@users.noreply.github.com>
2025-02-26 14:23:31 +0200

    Add files via upload

---
+++ RegistrationSytem.js
@@ -0,0 +1,17 @@
+let name =[];
+let ticketType;
+let registrationTime;
+
+functionfindEarlyRegistrations(participants, threshold){
+
+}
+function groupByTicketType(participants){
+return document.getElementsByName("$name").keys=ticketType;
+}
+function generateRegistrationDetails(participants){
+
+}
+function sortByRegistrationTime(participants) {
+
+}
+ 
